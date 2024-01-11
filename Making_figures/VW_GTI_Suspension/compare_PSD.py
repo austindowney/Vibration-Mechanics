383fc85007c9e43f742a92bc43bed10d91476cbf
Austin Downey <austindowney@gmail.com>
2024-01-10 21:19:04 -0500

    Chapter 1 Clean up

--- Making_figures/VW_GTI_Suspension/compare_PSD.py
+++ Making_figures/VW_GTI_Suspension/compare_PSD.py
@@ -52,12 +52,21 @@ with open('data/'+file_name+'.pkl', 'rb') as f:
 comfort_frequencies = loaded_dict['frequencies']
 comfort_psd = loaded_dict['psd']
 
+file_name = 'comfort_2'
+D1 = np.loadtxt('data/'+file_name+'.lvm',skiprows=23,max_rows=16000,delimiter='\t')
+comfort_tt = D1[:,0]
+comfort_aa = D1[:,1]
+
 file_name = 'normal_2'
 with open('data/'+file_name+'.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
 normal_frequencies = loaded_dict['frequencies']
 normal_psd = loaded_dict['psd']
 
+file_name = 'normal_2'
+D1 = np.loadtxt('data/'+file_name+'.lvm',skiprows=23,max_rows=16000,delimiter='\t')
+normal_tt = D1[:,0]
+normal_aa = D1[:,1]
 
 file_name = 'sport_2'
 with open('data/'+file_name+'.pkl', 'rb') as f:
@@ -65,7 +74,10 @@ with open('data/'+file_name+'.pkl', 'rb') as f:
 sport_frequencies = loaded_dict['frequencies']
 sport_psd = loaded_dict['psd']
 
-
+file_name = 'sport_2'
+D1 = np.loadtxt('data/'+file_name+'.lvm',skiprows=23,max_rows=16000,delimiter='\t')
+sport_tt = D1[:,0]
+sport_aa = D1[:,1]
 
 plt.subplots(figsize=(6, 2.5))
 plt.xlim([0,5])
@@ -81,6 +93,45 @@ plt.tight_layout()
 
 plt.savefig('data_plot.pdf')
 plt.savefig('VW_GTI_Suspension_2.jpg',dpi=250)
+
+linew = 0.8
+
+plt.figure(figsize=(6, 4.0))
+plt.subplot(211)
+plt.xlim([0,2])
+plt.grid('on')
+plt.plot(sport_tt, sport_aa,':',lw=0.9,label='sport',zorder=3)
+plt.plot(normal_tt, normal_aa,'--',lw=0.75,label='normal',zorder=2)
+plt.plot(comfort_tt, comfort_aa,lw=0.6,label='comfort',zorder=1)
+plt.xlabel("time (s)\n(a)")
+plt.ylabel('acceleraction (m/s$^2$)')
+
+
+leg = plt.legend(framealpha=1,ncol=3,loc=4)
+for line in leg.get_lines():
+    line.set_linewidth(1.2)
+#plt.legend(framealpha=1,ncol=3,loc=4)
+
+linew = 1.5
+
+plt.subplot((212))
+plt.xlim([0,5])
+plt.plot(sport_frequencies, sport_psd,':',lw=linew,label='sport')
+plt.plot(normal_frequencies, normal_psd,'--',lw=linew,label='normal')
+plt.plot(comfort_frequencies, comfort_psd,lw=linew,label='comfort')
+plt.xlabel("frequency (Hz)\n(b)")
+plt.ylabel('power spectral density\n(m$^2$/[s$^4 \cdot$ Hz])')
+#plt.legend(framealpha=1)
+# ax1.set_ylim([0,0.003])
+# ax2.set_ylim([0,0.000009])
+plt.tight_layout()
+
+plt.savefig('data_plot.pdf')
+plt.savefig('VW_GTI_Suspension_2.jpg',dpi=300)
+
+
+
+
 
 
 
